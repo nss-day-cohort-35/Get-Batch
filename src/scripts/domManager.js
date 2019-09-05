1180d9ca063148019ed81fa974d99b38e50f0a8e
Stephen Senft <stephensenft21@gmail.com>
2019-09-05 10:21:25 -0500

    adding classes and comments to DOM manager

--- src/scripts/domManager.js
+++ src/scripts/domManager.js
@@ -1,15 +1,22 @@
+/*This entire file is a form built by different variables holding the value of an object containing a method that takes in four parameters. the first parameter is an element type
+followed by text-content, ID, class and value.
+FIRST** The DOM is refenced by declaring displayContainer and then using the Javascript method "query selector" to obtain a refence to the div element on the DOM.
+Then, I begin to declare variables and give them values.
+Once the variables are declared, the input variables are given placeholders and the process of appending all the variables begins */
+
+
 // querying the DOM to get reference from an element with the ID of display-container
 const displayContainer = document.querySelector("#display-container")
 
-//div wrapper 
+//Creating a div wrapper  
 let divWrapper = htmlBuilder.createElementWithText("div", undefined, "div-wrapper", "wrapper", undefined)
 let mainHeader = htmlBuilder.createElementWithText("header", undefined, "main-header", "header", undefined)
 let h1Wrapper = htmlBuilder.createElementWithText("div", undefined, "h1-wrapper", "h1Wrapper", undefined)
 let h1 = htmlBuilder.createElementWithText("h1", "Welcome To Nashville", "H-One", undefined, undefined)
 
 //main body variable which contains the main content
-let mainBody = htmlBuilder.createElementWithText("main")
-let h2Wrapper = htmlBuilder.createElementWithText("div")
+let mainBody = htmlBuilder.createElementWithText("main",undefined,"main-body","")
+let h2Wrapper = htmlBuilder.createElementWithText("div",undefined,)
 let h2 = htmlBuilder.createElementWithText("h2", "Search")
 
 //Creating 1st & 2nd articles. The first article will hold the form containing fieldsets and user inputs 
@@ -36,6 +43,20 @@ let parksInput = htmlBuilder.createElementWithText("input")
 let concertsInput = htmlBuilder.createElementWithText("input")
 let eventsInput = htmlBuilder.createElementWithText("input")
 
+// Creating footer element containing a div, ul, 4 li and 4 anchor tags
+
+let footer = htmlBuilder.createElementWithText("footer")
+let linkWrapper = htmlBuilder.createElementWithText("div")
+let ul = htmlBuilder.createElementWithText("ul")
+let liOne = htmlBuilder.createElementWithText("li")
+let liTwo = htmlBuilder.createElementWithText("li")
+let liThree = htmlBuilder.createElementWithText("li")
+let liFour = htmlBuilder.createElementWithText("li")
+
+let a1 = htmlBuilder.createAnchorElement("a")
+let a2 = htmlBuilder.createAnchorElement("a")
+let a3 = htmlBuilder.createAnchorElement("a")
+let a4 = htmlBuilder.createAnchorElement("a")
 
 //input placeholders
 foodInput.placeholder = "search by food type"
@@ -76,3 +97,5 @@ form.appendChild(parksFieldSet).appendChild(parksInput)
 mainBody.appendChild(secondArticle)
 secondArticle.appendChild(resultsSection)
 secondArticle.appendChild(itinerarysection)
+
+
